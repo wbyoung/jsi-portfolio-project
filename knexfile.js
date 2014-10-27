89837333508f091541daf68502b2c40537f0768b
nonogon <leslie.strauss@gmail.com>
2014-10-27 14:36:32 -0700

    Set up local environment, migrated to latest.

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ var connection = {
   user: process.env.APP_DB_USER || '',
   password: process.env.APP_DB_PASSWORD || '',
   database: process.env.APP_DB_NAME || 'portfolio_test'
-}
+};
 
 module.exports = {
 
